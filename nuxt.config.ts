6c56807a87e3afa42b56fd46ffde5cf82e828bb7
Rémi Robichet <remi.robichet@edtake.com>
2024-05-13 12:17:30 +0200

    feat: add tailwind + add shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
   runtimeConfig: {
     public: {
       SUPABASE_URL: process.env.SUPABASE_URL,
       SUPABASE_ANNON_KEY: process.env.SUPABASE_ANNON_KEY,
     }
-  }
+  },
 })
