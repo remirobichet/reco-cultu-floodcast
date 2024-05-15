3b591c1ca013d26b168afcad134e451bb1af2125
Rémi Robichet <remi.robichet@edtake.com>
2024-05-15 11:51:56 +0200

    feat: replace `a` tag by `NuxtLink` for navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,18 +3,18 @@
     <header class="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav
         class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-        <a href="/" class="flex items-center gap-2 text-lg font-semibold md:text-base">
+        <NuxtLink to="/" class="flex items-center gap-2 text-lg font-semibold md:text-base">
           <Podcast class="h-6 w-6" />
           <span class="sr-only">Recommandations culturelles Floodcast</span>
-        </a>
-        <a href="/" class="text-foreground transition-colors hover:text-foreground">
+        </NuxtLink>
+        <NuxtLink to="/" exact-active-class="text-muted-foreground" class="text-foreground transition-colors hover:text-muted-foreground">
           Épisodes
-        </a>
-        <a href="/participants" class="text-muted-foreground transition-colors hover:text-foreground">
+        </NuxtLink>
+        <NuxtLink to="/participants" exact-active-class="text-muted-foreground" class="text-foreground transition-colors hover:text-muted-foreground">
           Participants
-        </a>
+        </NuxtLink>
       </nav>
-      <Sheet>
+      <Sheet v-model:open="navigationOpen">
         <SheetTrigger as-child>
           <Button variant="outline" size="icon" class="shrink-0 md:hidden">
             <Menu class="h-5 w-5" />
@@ -23,16 +23,16 @@
         </SheetTrigger>
         <SheetContent side="left">
           <nav class="grid gap-6 text-lg font-medium">
-            <a href="#" class="flex items-center gap-2 text-lg font-semibold">
+            <NuxtLink to="#" class="flex items-center gap-2 text-lg font-semibold">
               <Podcast class="h-6 w-6" />
               <span class="sr-only">Recommandations culturelles Floodcast</span>
-            </a>
-            <a href="/" class="hover:text-foreground">
+            </NuxtLink>
+            <NuxtLink to="/" exact-active-class="text-muted-foreground" class="text-foreground transition-colors hover:text-muted-foreground" @click="navigationOpen = false">
               Épisodes
-            </a>
-            <a href="/participants" class="text-muted-foreground hover:text-foreground">
+            </NuxtLink>
+            <NuxtLink to="/participants" exact-active-class="text-muted-foreground" class="text-foreground transition-colors hover:text-muted-foreground" @click="navigationOpen = false">
               Participants
-            </a>
+            </NuxtLink>
           </nav>
         </SheetContent>
       </Sheet>
@@ -52,4 +52,6 @@
 import { Menu, Podcast } from 'lucide-vue-next'
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+
+const navigationOpen = ref(false)
 </script>
