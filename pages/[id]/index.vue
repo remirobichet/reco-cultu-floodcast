746f87b56cab986204005b09177ad3caefe3962c
Rémi Robichet <remi.robichet@edtake.com>
2024-05-22 12:26:51 +0200

    feat: UI improve for recommentations

--- pages/[id]/index.vue
+++ pages/[id]/index.vue
@@ -1,32 +1,30 @@
 <template>
-  <div class="text-2xl font-bold line-clamp-3">
+  <div class="text-5xl font-bold line-clamp-3">
     {{ data.podcast.name }}
   </div>
-  <p class="text-xs text-muted-foreground line-clamp-3">
+  <p class="text-md text-muted-foreground line-clamp-3">
     {{ data.podcast.description }}
   </p>
-  <p class="text-xs">
+  <p class="text-md">
     {{ data.podcast.date }}
   </p>
 
   <div class="flex flex-row gap-4">
     <div class="flex flex-col gap-2">
-      <div class="text-lg font-medium">Participants</div>
-      <div class="flex flex-row gap-2">
-        <NuxtLink v-for="participant in data.participants" :to="`/participant/${participant.id}`">
-          {{ participant.name }}
-        </NuxtLink>
-      </div>
-    </div>
-  </div>
-
-  <div class="flex flex-row gap-4">
-    <div class="flex flex-col gap-2">
-      <div class="text-lg font-medium">Recommandations</div>
-      <div class="flex flex-row gap-2">
+      <div class="text-2xl font-medium mb-8">Recommandations</div>
+      <div class="flex flex-col gap-4">
         <div v-for="recommendation in data.recommendations">
-          <div>{{ recommendation.participant.name }} :</div>
-          <div>{{ recommendation.text }}</div>
+          <div class="flex items-center gap-2">
+            <Avatar class="flex h-9 w-9">
+              <AvatarImage :src="recommendation.participant.avatar"
+                :alt="`${recommendation.participant.name} avatar`" />
+              <AvatarFallback>{{ recommendation.participant.name[0] }}</AvatarFallback>
+            </Avatar>
+            <p class="text-lg font-bold">{{ recommendation.participant.name }}</p>
+          </div>
+          <blockquote class="p-4 my-4 border-s-4">
+            <pre v-html="recommendation.text" class="text-xl leading-relaxed font-sans whitespace-break-spaces"></pre>
+          </blockquote>
         </div>
       </div>
     </div>
@@ -49,7 +47,6 @@ const { data } = await useAsyncData(
   async () => {
     const response = {
       podcast: undefined,
-      participants: [],
       recommendations: [],
     }
 
@@ -64,15 +61,6 @@ const { data } = await useAsyncData(
 
     if (podcast) {
       response.podcast = podcast
-    }
-
-    const { data: participants } = await $supabase
-      .from('podcast_participant')
-      .select('*, participant!inner (*)')
-      .eq('podcast_id', podcast.id)
-
-    if (participants) {
-      response.participants = participants.map((participant) => participant.participant)
     }
 
     const { data: recommendations } = await $supabase
