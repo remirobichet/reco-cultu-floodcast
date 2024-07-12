6f77cb53bcad1a060609c55a55d393e9ff8cc18c
Rémi Robichet <remi.robichet@edtake.com>
2024-07-12 10:00:47 +0200

    fix: menu item no longer shink

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="flex min-h-screen w-full flex-col">
     <header class="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav
-        class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        class="hidden shrink-0 flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <NuxtLink to="/" class="flex items-center gap-2 text-lg font-semibold md:text-base">
           <Podcast class="h-6 w-6" />
           <span class="sr-only">Recommandations culturelles Floodcast</span>
